1ea99fe4c22fe622181aa24d37ab40c8dbf7445b
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-03-22 21:32:07 +0100

    remove the ger colors

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -17,18 +17,6 @@
     header {
         background-color: #E1E3E6;
         position: relative;
-        margin-bottom: 3px;
-    }
-
-    header::after {
-        content: '';
-        display: block;
-        width: 100%;
-        height: 3px;
-        position: absolute;
-        left: 0;
-        top: 0;
-        background: linear-gradient(to right, #000000 33%, #de0000 33.001%, #de0000 66%, #ffcf00 66.001%);
     }
 </style>
 
