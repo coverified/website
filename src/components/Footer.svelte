0f1a39da55d12b27cc733bffab5fd33845d2297f
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-03-24 18:13:29 +0100

    add netlify badge

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,10 +2,21 @@
 </script>
 
 <style>
-    footer{
+    footer {
         max-width: 71.25rem;
         padding: 2rem 1rem;
         margin: 0 auto;
+    }
+
+    .box {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .netlify-attribution {
+        display: block;
     }
 </style>
 
@@ -18,7 +29,11 @@
         <small>
             Created during the
             <a href="https://twitter.com/hashtag/WirVsVirusHack" target="_blank" rel="noopener">#wirvsvirus</a>
-            <a href="https://devpost.com/software/1_039_c_staatlichekommunikation_webinfowidget" target="_blank" rel="noopener">hackathon</a>
+            <a href="https://devpost.com/software/1_039_c_staatlichekommunikation_webinfowidget" target="_blank"
+               rel="noopener">hackathon</a>
         </small>
+        <a href="https://www.netlify.com" class="netlify-attribution">
+            <img src="https://www.netlify.com/img/global/badges/netlify-light.svg" alt="Deploys by Netlify"/>
+        </a>
     </p>
 </footer>
