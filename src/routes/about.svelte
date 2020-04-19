badbe57e9fa23f63e864371a4e0cce1bf8d25cdc
steven4714 <s.killian@ellusion.de>
2020-04-19 19:08:12 +0200

    added content pages

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,6 +9,59 @@
     padding-right: 1.5rem;
   }
 
+  header {
+    background-color: var(--color-primary);
+    height: 18vh;
+    position: relative;
+    justify-content: center;
+    display: flex;
+    margin-bottom: 3rem;
+    
+    &:before {
+        content: "";
+        position: absolute;
+        background: var(--color-primary);
+        display: block;
+        width: 100vw;
+        height: 100%;
+        left: 50%;
+        right: 0;
+        bottom: 0;
+        top: 0;
+        transform: translateX(-50%);
+      }
+    }
+
+    h1 {
+        font-size: 2rem;
+        position: relative;
+        color: var(--color-white);
+        top: 68%;
+        left: 1.8%;
+    }
+
+    .wirvsvirus__logo {
+        position: absolute;
+        width: 20rem;
+        bottom: 37%;
+        right: 1.125rem;
+    }
+
+  p {
+    margin-bottom: 2.5rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+    color: var(--color-primary);
+  }
+
+  h3 {
+    font-size: 1.625rem;
+    margin-top: 5.0625rem;
+    margin-bottom: .6875rem;  
+  }
+
   div {
     display: flex;
     justify-content: space-between;
@@ -31,10 +84,29 @@
     }
   }
   @media (max-width: 768px) {
+    header {
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
     
+      > h1 {
+          top: 0;
+      }
+
+      > .wirvsvirus__logo {
+          bottom: 1rem;
+          right: auto;
+          width: 20rem;
+      }
+    }
   }
 
   @media (max-width: 576px) {
+    
+    h2 {
+            font-size: 1.575rem;
+        }
+
     div {
       flex-direction: column;
       align-items: center;
@@ -51,59 +123,49 @@
 	<title>Information</title>
 </svelte:head>
 
-<Header pageName={'Informationen'}/>
+<header>
+    <h1 class="sr-only">das Projekt</h1>
+    <h1>
+        das Projekt
+    </h1>
+    <img class="wirvsvirus__logo" src="wirvsvirus-weiss.svg" alt="Wir vs Virus Projekt">
+</header>
 <main>
   <h2>
-    So funktioniert CoVerfied
+    Ein Projekt entstanden beim Hackathon<br class="d-none d-lg-block"/> der Bundesregierung im März 2020.
   </h2>
+  <h3>
+    Wir sind ein von der Bundesregierung gefördertes Projekt
+  </h3>
   <p>
-    CoVerified sammelt Daten aus verifizierten Quellen im Internet, bündelt diese in einer Datenbank 
-    und gibt sie als News-Vorschau über unser Informations-Tool (der Button auf Ihrer Website unten rechts) 
-    aus. Es gibt noch kein vergleichbares Angebot, das zentral die wichtigsten, validierten Daten bündelt 
-    wie zum Beispiel vom Robert-Koch-Institut, der Bundesregierung und dem Ministerium für Gesundheit. 
-    Wir wollen sicher gehen, dass wirklich nur Informationen aus wissenschaftlichen oder staatlichen 
-    Quellen genutzt werden.
-  </p>
-  <div>
-    <img class="coverified__informationgraphic" src="coverified-information-graphic.svg" alt="Coverified Information graphic">
-    <p>
-      Unsere Datenbank wird gefüttert mit allen aktuellen Daten aus den RSS-Feeds offizieller Stellen. Diese 
-      Informationen werden als News-Vorschau in unserem Tool dargestellt. Klickt oder tippt der Nutzer auf ein 
-      solches Vorschau-Feld, wird er direkt zur aktuellen Meldung auf die Seite der jeweiligen Quelle weitergeleitet. 
-    </p>
-  </div>
-  <h2>
-    Proaktiv gegen Falschmeldungen (Fake News) im Netz
-  </h2>
-  <p>
-    Unser Ziel ist es, proaktiv gegen Falschmeldungen zu arbeiten in dem wir auf (möglichst) vielen Internetangeboten 
-    verifizierte Informationen zur Verfügung stellen. Damit schaffen wir eine umfängliche Information der Bevölkerung 
-    und damit Vertrauen in politische Entscheidungen. Es geht dabei nicht um die kontinuierliche Information der 
-    Bürger*innen über das polit. Zeitgeschehen, sondern ausdrücklich um das Bereitstellen von wissenschaftl. überprüften 
-    Fakten in Ausnahmesituationen. Das Widget kann auch zu einem späteren Zeitpunkt auf andere Krisen angepasst werden 
-    wie z.B. Naturkatastrophen. 
-  </p>
-  <h2>
-    Wir sind ein Projekt des #WirvsVirus Hackathons
-  </h2>
-  <p>
-    Die Idee zu unserem Informations-Tool ist beim #WirvsVirus Hackathon entstanden. Der Hackathon war eine Veranstaltung im 
-    Internet der Bundesregierung. Dort wurden Lösungen für die aktuelle Covid19-Krise erarbeitet und programmiert. Sie sehen 
-    hier unsere Idee und den aktuellen Stand. Unser Code-Schnipsel kann bereits in Websites eingebunden werden und unsere 
-    Datenbank sammelt aktuelle Informationen. 
+    Die Idee zu unserem Informations-Tool ist beim #WirvsVirus Hackathon entstanden. Der Hackathon wurde von der Bundesregierung initiiert. Es wurden Lösungen für die aktuelle Covid19-Krise via Internet von neu gebildeten Teams aus interessierten Bürger*innen erarbeitet und programmiert. CoVerified war die Idee unseres Teams. Unser Code-Schnipsel kann in Websites eingebunden werden, um Besucher*innen aktuelle Informationen verifizierter Quellen aus unserer Datenbank bereitzustellen.
   </p>
   <p>
-    Wir arbeiten stetig weiter an unserem Projekt und freuen uns über Lib, gleich wohl wie Kritik, Feedback, Anregungen 
-    oder auch WoMan-Power. 
+    Wir arbeiten stetig weiter an unserem Projekt und freuen uns über Lob, gleich wohl wie Kritik, Feedback, Anregungen oder auch WoMan-Power.
   </p>
   <p>
-    Unser Projekt ist eine Non-Profit Lösung, unser Code wurde in OpenSource geschrieben. 
+    Unser Projekt ist eine Non-Profit Lösung, unser Code wurde in OpenSource geschrieben.
   </p>
   <p>
-    Unsere Projektbeschreibung und die Dokumentation beim #WirvsVirus Hackathon finden Sie hier:
+    Unsere Projektbeschreibung und die Dokumentation beim #WirvsVirus Hackathon finden Sie hier: 
     <a href="https://devpost.com/software/1_039_c_staatlichekommunikation_webinfowidget" title="Projektbeschreibung" target=“_blank“ rel=“noopener“>
       Devpost
     </a>
+  </p>
+  <h3>
+    Unsere Motivation
+  </h3>
+  <p>
+    Unser Ziel ist es, proaktiv gegen Falschmeldungen zu arbeiten in dem wir ein Tool zur Verfügung stellen, welches Informationen aus seriösen Quellen zieht und in einem Widget bündelt. So schaffen wir es, mit nur einem Tool, umfängliche Informationen zu Covid-19 bereitzustellen. Es werden Informationen aus den einzelnen Bundesländern, medizinische Ratschläge, Erkenntnisse aus der Forschung oder Verhaltensratschläge gebündelt und aktuell zur Verfügung gestellt. Dabei geht es uns vor allem um das Bereitstellen von wissenschaftl. überprüften Fakten in Ausnahmesituationen. So kann das Widget auch zu einem späteren Zeitpunkt auf andere Krisen angepasst werden z. B. bei Naturkatastrophen.
+  </p>
+  <h3>
+    So funktioniert es
+  </h3>
+  <p>
+    CoVerified sammelt Daten aus verifizierten Quellen im Internet, bündelt diese in einer Datenbank und gibt sie als News-Vorschau über unser Informations-Tool (der Button auf Ihrer Website unten rechts) aus. Es gibt noch kein vergleichbares Angebot, das zentral die wichtigsten, validierten Daten bündelt wie zum Beispiel vom Robert-Koch-Institut, der Bundesregierung und dem Ministerium für Gesundheit. Wir wollen sicher gehen, dass wirklich nur Informationen aus wissenschaftlichen oder staatlichen Quellen genutzt werden.
+  </p>
+  <p>
+    Unsere Datenbank wird gefüttert mit allen aktuellen Daten aus den RSS-Feeds offizieller Stellen. Diese Informationen werden als News-Vorschau in unserem Tool dargestellt. Klickt oder tippt der Nutzer auf ein solches Vorschau-Feld, wird er direkt zur aktuellen Meldung auf die Seite der jeweiligen Quelle weitergeleitet.
   </p>
 </main>
 
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,26 +1,109 @@
 <script>
   import Header from "../components/Header.svelte";
+  import Col from 'sveltestrap/src/Col.svelte';
+  import Row from 'sveltestrap/src/Row.svelte';
 </script>
 
 <style type="text/scss">
+  header {
+        background-color: var(--color-primary);
+        height: 18vh;
+        position: relative;
+        justify-content: center;
+        display: flex;
+        margin-bottom: 3rem;
+        
+        &:before {
+            content: "";
+            position: absolute;
+            background: var(--color-primary);
+            display: block;
+            width: 100vw;
+            height: 100%;
+            left: 50%;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            transform: translateX(-50%);
+            }
+    }
 
+    h1 {
+        font-size: 2rem;
+        position: relative;
+        color: var(--color-white);
+        top: 68%;
+        left: 1.8%;
+    }
+
+    .wirvsvirus__logo {
+        position: absolute;
+        width: 20rem;
+        bottom: 37%;
+        right: 1.125rem;
+    }
+  
+  h2 {
+    font-size: 2rem;
+    color: var(--color-primary);
+    margin-bottom: 2.5rem;
+  }
+
+  h3 {
+    margin-bottom: .9375rem;
+  }
+
+  p {
+    margin-bottom: 2.5rem;
+  }
+
+  @media (max-width: 768px) {
+    header {
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        // height: 24vh;
+        
+        > h1 {
+            top: 3px;
+        }
+
+        > .wirvsvirus__logo {
+            bottom: 1rem;
+            right: auto;
+            width: 15rem;
+        }
+    }
+  }
 </style>
 
 <svelte:head>
 	<title>Kontakt</title>
 </svelte:head>
+<header>
+  <h1 class="sr-only">Kontakt</h1>
+  <h1>
+      Kontakt
+  </h1>
+  <img class="wirvsvirus__logo" src="wirvsvirus-weiss.svg" alt="Wir vs Virus Projekt">
+</header>
+<Row>
+  <Col sm="auto" lg="5">
+<h2>
+  Ideen? Kooperationen? Fragen?
+</h2>
 
-<Header pageName={'Kontakt'} />
+<h3>
+  Einfach eine mail schreiben!
+</h3>
 <p>
-  Unser neunköpfiges Team, steht Euch gerne Rede und Antwort! 
-  Meldet Euch gerne mit Feedback, Anfragen und Anregungen. 
+  Unser neunköpfiges Team, steht Euch gerne Rede und Antwort! Meldet Euch gerne mit Feedback, Anfragen und Anregungen.
 </p>
 <p>
-  <b>
-    Wir sind erreichbar unter
-    <a href= "mailto:info@coverified.info" title="CoVerified E-Mail">info@coverified.info</a>
-  </b>
+  Wir sind erreichbar unter <a href= "mailto:info@coverified.info" title="CoVerified E-Mail">info@coverified.info</a>
 </p>
+</Col>
+</Row>
 
 
 
